137d4f484bf0a516f2f54996f6b095273f36364f
Tyler <tdoermann@gmail.com>
2017-01-23 18:51:50 -0700

    skip to end, average

--- assets/roller.js
+++ assets/roller.js
@@ -1,6 +1,22 @@
 
+//Keep track of timeouts so we can get rid of them if user skips to results
+var timeouts = [];
+
+function clear_timeouts() {
+	//Get rid of all timeouts if they skip to the results
+	$.each(timeouts, function(){
+		window.clearTimeout(this);
+	});
+
+}
 
 $(document).ready(function(){
+	$(document).on('click', '#skip', function(){
+		$('#loading').remove();
+		$("#rollResults").show();
+		clear_timeouts();
+	});
+
 	$('#roller').click(function(){
 		$("#rollTable").empty();
 		$("#rollResults").hide();
@@ -16,6 +32,7 @@ $(document).ready(function(){
 		var loading_str = 
 		'<div id="loading"> ' +
 		'<h2 style="display:none;" id="roll_amount"></h2>' +
+		'<button type="button" id="skip">Skip to Results</button>' + 
 		'</div>';
 				
 		$('body').append(loading_str);
@@ -28,7 +45,10 @@ $(document).ready(function(){
 			end = false;
 			if (i == numDice) {
 				end = true;
-				$("#rollTable").append("<tr><th>Total</th><th>" + totalRoll + "</th></tr>");
+				$("#rollTable").append(
+					"<tr><th>Total</th><th>" + totalRoll + "</th></tr>" + 
+					"<tr><th>Average</th><th>" + (totalRoll/numDice).toFixed(2) + "</th></tr>"
+					);
 			}
 			
 			display_roll(diceRoll, i, end);
@@ -36,18 +56,20 @@ $(document).ready(function(){
 	});
 });
 
+
+//this function displays the result of a roll one at a time for each roll
 function display_roll(diceRoll, iter, end) {
 	
-	setTimeout(function(){ 
+	timeouts.push(setTimeout(function(){ 
 		$('#roll_amount').hide().html("<i><u>Roll #" + iter + "</u></i><br>" + diceRoll).fadeIn();
 
-	}, (iter * 2000));
+	}, (iter * 2000)));
 
 	if (end == true) {
-		setTimeout(function(){
+		timeouts.push(setTimeout(function(){
 			$('#loading').remove();
 			$("#rollResults").show();
-		}, ((1+iter) * 2000));
+		}, ((1+iter) * 2000)));
 		
 	}
 	
